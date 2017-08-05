057a6a5ed733ec9a3689550a4b61caf8018c4955
kotatuinu <kotatuinu@nifty.com>
2017-08-05 17:36:36 +0900

    created

---
+++ watchFile/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace watchFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSetDir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnSetDir
+            // 
+            this.btnSetDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSetDir.Location = new System.Drawing.Point(17, 352);
+            this.btnSetDir.Name = "btnSetDir";
+            this.btnSetDir.Size = new System.Drawing.Size(97, 23);
+            this.btnSetDir.TabIndex = 1;
+            this.btnSetDir.Text = "ディレクトリ指定";
+            this.btnSetDir.UseVisualStyleBackColor = true;
+            this.btnSetDir.Click += new System.EventHandler(this.btnSetDir_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(120, 352);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(649, 23);
+            this.label1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(757, 325);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 393);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSetDir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSetDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ watchFile/Form1.cs
@@ -0,0 +1,133 @@
+﻿/* 
+ * 参考：DOBON.NET > プログラミング道 > .NET Tips > ファイル、フォルダ「フォルダ、ファイルの変更を監視する」
+ * http://dobon.net/vb/dotnet/file/filesystemwatcher.html
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace watchFile
+{
+    public partial class Form1 : Form
+    {
+        private System.IO.FileSystemWatcher watcher_ = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetDir_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                listView1.Items.Clear();
+                label1.Text = folderBrowserDialog1.SelectedPath;
+                start(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        //Button1のClickイベントハンドラ
+        private void start(string path)
+        {
+            if (watcher_ != null)
+            {
+                stop();
+            }
+
+            watcher_ = new System.IO.FileSystemWatcher();
+            //監視するディレクトリを指定
+            watcher_.Path = path;
+            //最終アクセス日時、最終更新日時、ファイル、フォルダ名の変更を監視する
+            watcher_.NotifyFilter =
+                (System.IO.NotifyFilters.LastAccess
+                | System.IO.NotifyFilters.LastWrite
+                | System.IO.NotifyFilters.FileName
+                | System.IO.NotifyFilters.DirectoryName);
+            //すべてのファイルを監視
+            watcher_.Filter = "";
+            watcher_.IncludeSubdirectories = true;
+            //UIのスレッドにマーシャリングする
+            //コンソールアプリケーションでの使用では必要ない
+            watcher_.SynchronizingObject = this;
+
+            //イベントハンドラの追加
+            watcher_.Changed += new System.IO.FileSystemEventHandler(watcher_Changed);
+            watcher_.Created += new System.IO.FileSystemEventHandler(watcher_Changed);
+            watcher_.Deleted += new System.IO.FileSystemEventHandler(watcher_Changed);
+            watcher_.Renamed += new System.IO.RenamedEventHandler(watcher_Renamed);
+
+            //監視を開始する
+            watcher_.EnableRaisingEvents = true;
+            Console.WriteLine("監視を開始しました。");
+        }
+
+        //Button2のClickイベントハンドラ
+        private void stop()
+        {
+            //監視を終了
+            watcher_.EnableRaisingEvents = false;
+            watcher_.Dispose();
+            watcher_ = null;
+            Console.WriteLine("監視を終了しました。");
+        }
+
+        //イベントハンドラ
+        private void watcher_Changed(System.Object source, System.IO.FileSystemEventArgs e)
+        {
+            ListViewItem item1;
+            switch (e.ChangeType)
+            {
+                case System.IO.WatcherChangeTypes.Changed:
+                    item1 = new ListViewItem("Changed", 0);
+                    item1.SubItems.Add(e.FullPath);
+                    listView1.Items.Add(item1);
+
+                    Console.WriteLine(
+                        "ファイル 「" + e.FullPath + "」が変更されました。");
+                    break;
+                case System.IO.WatcherChangeTypes.Created:
+                    item1 = new ListViewItem("Created", 0);
+                    item1.SubItems.Add(e.FullPath);
+                    listView1.Items.Add(item1);
+
+                    Console.WriteLine(
+                        "ファイル 「" + e.FullPath + "」が作成されました。");
+                    break;
+                case System.IO.WatcherChangeTypes.Deleted:
+                    item1 = new ListViewItem("Deleted", 0);
+                    item1.SubItems.Add(e.FullPath);
+                    listView1.Items.Add(item1);
+
+                    Console.WriteLine(
+                        "ファイル 「" + e.FullPath + "」が削除されました。");
+                    break;
+            }
+        }
+
+        private void watcher_Renamed(System.Object source, System.IO.RenamedEventArgs e)
+        {
+            ListViewItem item1 = new ListViewItem("Renamed", 0);
+            item1.SubItems.Add(String.Format("{0} → {1}", e.OldFullPath, e.FullPath));
+            listView1.Items.Add(item1);
+
+            Console.WriteLine(
+                "ファイル 「" + e.FullPath + "」の名前が変更されました。");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            ListViewItem item1 = new ListViewItem("Event", 0);
+            listView1.Columns.Add("Evant", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("FileName", -2, HorizontalAlignment.Left);
+
+        }
+    }
+}
